62a8e2053f03b39c68602c1c6762e6b033ed69e3
EmmaFrx <emma.farineaux@epitech.eu>
2024-02-29 09:31:08 +0100

    [feat] -->  add the '-h'

---
+++ src/display_usage.py
@@ -0,0 +1,7 @@
+def display_usage() -> int:
+    print("USAGE")
+    print("\t ./107transfer [num den]+")
+    print("DESCRIPTION")
+    print("\t num \t polynomial numerator defined by its coefficients")
+    print("\t den \t polynomial denominator defined by its coefficients")
+    return 0
