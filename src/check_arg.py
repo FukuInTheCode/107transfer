fe60396b8ba8eea9d38e258a473c6f5679ca15d3
EmmaFrx <emma.farineaux@epitech.eu>
2024-03-11 11:21:27 +0100

    [feat] -->  add the error handling

--- src/check_arg.py
+++ src/check_arg.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+from argv_to_array import argv_to_arrays
+
 
 def is_float(num):
     try:
@@ -12,4 +14,10 @@ def is_float(num):
 def check_arg(abrs: list[str]) -> int:
     if len(abrs) % 2 != 0:
         exit(84)
+    for i in range(0, len(abrs), 2):
+        if int(abrs[i + 1]) == 0:
+            exit(84)
+        if is_float(abrs[i]):
+            exit(84)
+
     return 0
